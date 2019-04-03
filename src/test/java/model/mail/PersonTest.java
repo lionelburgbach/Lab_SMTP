dd4bab0a0244c53306b45f2cfa74d7f6a7c88145
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-04-03 14:01:50 +0200

    Add PersonTest

---
+++ src/test/java/model/mail/PersonTest.java
@@ -0,0 +1,23 @@
+package model.mail;
+
+import configuration.ConfigurationManager;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void firstPersonShouldBeOk() throws IOException {
+
+        ConfigurationManager configurationManager = new ConfigurationManager();
+        List<Person> list = configurationManager.getVictims();
+
+        assertEquals(list.get(0).getFirstName(), "Alexandre");
+        assertEquals(list.get(0).getLastName(), "Gabrielli");
+        assertEquals(list.get(0).getAddress(), "alexandre.gabrielli@heig-vd.ch");
+    }
+}
