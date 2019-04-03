ada8c99dd504a513ebc8a1982cc78c0a7ad046d6
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-04-03 13:50:33 +0200

    Add DockerMockMock

---
+++ DockerMockMock/build-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image locally
+docker build --tag java-server-mockmock .
---
+++ DockerMockMock/run-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+##
+docker run -d -p 8080:8080 -p 25000:25000 java-server-mockmock
