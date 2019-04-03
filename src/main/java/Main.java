25788e7619f539bdebdbde0aa9c419d45008a65e
ncdm-stldr <ncdm.stldr@gmail.com>
2019-04-03 19:52:56 +0200

    some progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,31 @@
+import model.mail.Person;
+import model.prank.Prank;
+import protocol.SmtpClient;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args){
 
+        Person kim = new Person("Kim", "Jong-un", "Kim.Jung-un@korea-dpr.com");
+        Person trump = new Person("Donald", "Trump", "donald.trump@whitehouse.gov");
+        Person trumpVariant = new Person("Donald", "Trump", "trump@whitehouse.gov");
+        Person randomGuy = new Person("Random", "Guy", "guy@random.random");
+        List<Person> victims = new LinkedList<>();
+        victims.add(trump);
+        victims.add(trumpVariant);
+        List<Person> witnesses = new LinkedList<>();
+        witnesses.add(randomGuy);
+        String message = "...";
+
+        Prank prank = new Prank(kim, victims, witnesses, message);
+        List<Prank> pranks = new LinkedList<>();
+        pranks.add(prank);
+
+        SmtpClient smtp = new SmtpClient("127.0.0.1", 2323, pranks);
+        smtp.sendEmail();
 
 
     }
--- src/main/java/protocol/SmtpClient.java
+++ src/main/java/protocol/SmtpClient.java
@@ -2,9 +2,12 @@ package protocol;
 
 import model.prank.Prank;
 
+import java.io.*;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+
 
 public class SmtpClient implements ISmtpClient {
 
@@ -13,8 +16,11 @@ public class SmtpClient implements ISmtpClient {
     private String smtpServerIPAddress;
     private int smtpServerPort;
 
-
     private List<Prank> prank;
+
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
 
     public SmtpClient(String smtpServerIPAddress, int port, List<Prank> prank){
 
@@ -25,9 +31,36 @@ public class SmtpClient implements ISmtpClient {
 
     @Override
     public void sendEmail() {
-        //TODO
 
 
+        try {
+            LOG.info("Sending message via SMTP");
+
+            socket = new Socket(smtpServerIPAddress, smtpServerPort);
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+            reader.readLine();
+
+            writer.write(Protocol.CMD_HELLO + "\r\n");
+            writer.flush();
+
+            String line = reader.readLine();
+            System.out.println(line);
+
+            //note: maybe this is not general enough
+            while (line.startsWith("250-")){
+                line = reader.readLine();
+                System.out.println(line);
+            }
+
+            System.out.println("(our turn now)");
+
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
 
 
         return;
